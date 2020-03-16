d8adb0e0613ec78b6d089776cabacd1f4dff5b8f
Arlliden <vodkabotle@gmail.com>
2020-03-16 21:43:24 +0100

    Updates

---
+++ precache-manifest.8279a114c5bab3ba3df735b3b8f3caaa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.8c97409f.js"
+  },
+  {
+    "revision": "d355e0c97a18ca3be5db",
+    "url": "./static/js/main.d355e0c9.chunk.js"
+  },
+  {
+    "revision": "59826417d8abc840b5e9",
+    "url": "./static/js/2.59826417.chunk.js"
+  },
+  {
+    "revision": "872234b6146d13d776645ede252ef6c4",
+    "url": "./index.html"
+  }
+];
